c4cdc870429a4d6217366d94f3700970108d62cb
Abrar <darabrar@outlook.com>
2020-03-09 22:44:44 +0530

    Updates

---
+++ precache-manifest.aa11da2f62a6f21b15ce2c652246c7ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4bb72380a1e403f162a2cac39c11034",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "99a46a7def36cf2e1779",
+    "url": "/weather/static/css/main.56ee10f2.chunk.css"
+  },
+  {
+    "revision": "3cf406e489a4359171f0",
+    "url": "/weather/static/js/2.c5b7eaec.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/weather/static/js/2.c5b7eaec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99a46a7def36cf2e1779",
+    "url": "/weather/static/js/main.8f9feb6c.chunk.js"
+  },
+  {
+    "revision": "69c831f7728627c1cae5",
+    "url": "/weather/static/js/runtime-main.d7f8fb79.js"
+  }
+]);
